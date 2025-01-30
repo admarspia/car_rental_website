a7dc421a294041e797ab216c2f48cfc68c4498a4
admarspia <145662971+admarspia@users.noreply.github.com>
2025-01-30 09:10:51 -0500

    Update RegisterPage.js

--- front-end/src/components/RegisterPage.js
+++ front-end/src/components/RegisterPage.js
@@ -17,7 +17,7 @@ function Register() {
   };
   
   const handleSubmit = async (e) => {
-    e.preventDefault(); // Prevent page reload
+    e.preventDefault(); 
     try {
       const response = await fetch("http://localhost:5000/customers/register", {
         method: "POST",
